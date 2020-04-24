45431a3d5a26657374a3f2734aa0d8c811c2da12
Todd Burns <toddnby@gmail.com>
2020-04-24 09:30:51 -0400

    Week12HW
    C:\Users\tburns\Documents\GitHub\mcc_tools_tech\Week12-TranslationAccessibility\ToddHW

---
+++ Week11-DITA/lisa_fleischmann_week11_homework/Test DITA Acme Eye1/Output/Karana/HTML5/Data/Glossary_Chunk0.js
@@ -0,0 +1,1 @@
+define({'Consectetur':{d:'Definition for consectetur.',l:''},'Ipsum':{d:'Definition for ipsum.',l:''},'Lorem':{d:'Definition for lorem.',l:''},'Maecenas':{d:'Definition for maecenas.',l:''},'Maximus':{d:'Definition for maximus.',l:''}});

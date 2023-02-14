164dfd61f2349be397816b44e10658ceec427e36
Zoë Lawson <zoelawson17@hotmail.com>
2023-02-13 22:01:15 -0500

    Setup for Week 4

---
+++ Week04-Tours/Homework/SP_readtour.js
@@ -0,0 +1,21 @@
+function SPstartTour() {
+	introJs().setOptions({
+	  steps: [{
+		title: 'Welcome Sarim',
+		intro: 'This is a sample tour using Intro.js.'
+	  },
+	  {
+		element: document.querySelector('#step1'),
+		intro: 'You could change this image in week 2 using the Graphic property.'
+	  },
+	  {
+		element: document.querySelector('#step2'),
+		intro: 'The text this help icon triggers the text in the UserName_Help property.'
+	  },
+	  {
+		title: 'Farewell!',
+		element: document.querySelector('#step3'),
+		intro: 'Double-click this button to see some silly text.'
+	  }]
+	}).start();
+}
